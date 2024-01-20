47d3034d97ede3e6b6b1312aaaed6c75fa3125eb
Philipp Trulson <der-eismann@users.noreply.github.com>
2024-01-20 12:03:45 +0100

    Print number of duplicates

--- dist/index.js
+++ dist/index.js
@@ -26581,7 +26581,7 @@ try {
     });
     let output;
     if (conflicts.size > 1) {
-        output = 'Duplicates detected:';
+        output = `${conflicts.size} duplicates detected:`;
         conflicts.forEach((value, key) => {
             output = output.concat('\n', `  - File: ${key}\n    Packages:`);
             value.forEach(value => {
